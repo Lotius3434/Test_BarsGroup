050b490036c1aa3ed1afea6d1c817734b41ee46a
Lotius3434 <koryaginlotius2@gmail.com>
2020-04-02 01:08:58 +0300

    Удалил старые классы WorkServer

--- TestBars/Program.cs
+++ TestBars/Program.cs
@@ -56,68 +56,17 @@ namespace TestBars
         {
             var container = new WindsorContainer();
             container.Install(new ConfigurationCastleWindsor());
-            List<IList<IList<Object>>> ListInfo; //Главный спиcок, который хранит в себе все сервера и их данные.
-
-            //IContainerServers containerServers = new ContainerServers(); //Контейнер, который создает и хранит в себе список серверов.
-
-            //if (containerServers.CreateServers())
-            //{
-            //    List<IServer> servers = containerServers.GetServers;
-
-            //    for (int a = 0; a < servers.Count; a++)
-            //    {
-            //        servers[a].OpenConnection(); // Открытие соединения с серверами.
-            //    }
-
-
-            //    ListInfo = new List<IList<IList<Object>>>();
-            //    for (int a = 0; a < servers.Count; a++)
-            //    {
-
-            //        ListInfo.Add(servers[a].GetBasesandSizes());//Добавление данных из баз серверов.
-            //    }
-
-
-
-            //    for (int a = 0; a < servers.Count; a++)
-            //    {
-            //        servers[a].CloseConnection(); // Закрытие соединения с серверами.
-            //    }
-
-
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Конфигурации не найдены, заполните данные сервера\nДля выхода из программы, нажмите 'q'\n");
-            //    return;
-            //}
-
-
-            //UserAuthentication userAuthentication = new UserAuthentication(); // Аутенфикация пользователя.
-            //Services services = new Services();  //Создание сервисов.
-            //Config_Scope_Appname.credential = userAuthentication.Authentication(); //Добавление списка полномочий в общий стат класс.
-
-            //SheetsService sheetsService = services.GetSheetsService(); //Получение сервиса для гугл таблиц. 
-            //DriveService driveService = services.GetDriveService(); //Получение сервиса для гугл диска. 
+            
             var connection = container.Resolve<IConnectionDb>();
             IList<IServerObj> servers = connection.GetServers();
 
             IServices services = container.Resolve<IServices>();
-            
             ISpreasheet spreasheet = container.Resolve<ISpreasheet>();
             ISearchSpreadsheets searchSpreadsheets = container.Resolve<ISearchSpreadsheets>();
 
             spreasheet.sheetsService = services.GetSheetsService();
             searchSpreadsheets.driveService = services.GetDriveService();
-            // Реализация интерфейса, который содержит в себе весь фунционал работы с таблицами и листами.
-
-
-
-
-
-
-
-
+            
 
 
             string listIdSreadssheet = searchSpreadsheets.Search(); //Поиск таблиц на гугл диске.
--- TestBars/WorkServersPostgreSql/IParseConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TestBars.WorkServersPostgreSql
-{
-    interface IParseConfiguration
-    { 
-        Dictionary<string, string> GetConfigServers_Npgsql();
-    }
-}
